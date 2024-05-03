309e332d406e337ed56f2698eb163cb0207cd64b
Ismael Mendoza <imendoza@umich.edu>
2024-05-03 09:28:55 -0400

    latest deblender with more epochs and bigger batch size

--- case_studies/galsim_galaxies/run/run_deblender.sh
+++ case_studies/galsim_galaxies/run/run_deblender.sh
@@ -3,6 +3,6 @@
 export CUDA_VISIBLE_DEVICES="5"
 
 echo >> log.txt
-cmd="./bin/run_deblender_train.py -s 43 -t "15_42""
+cmd="./bin/run_deblender_train.py -o -s 44 -t "15_44""
 echo $cmd >> log.txt
 eval $cmd
