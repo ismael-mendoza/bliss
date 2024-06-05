dbc375a36c1b3d4ae3326cbc40bb696304f1571c
Ismael Mendoza <imendoza@umich.edu>
2024-06-05 16:57:00 -0400

    new dataset

--- case_studies/galsim_galaxies/run/run_deblender.sh
+++ case_studies/galsim_galaxies/run/run_deblender.sh
@@ -3,6 +3,6 @@
 export CUDA_VISIBLE_DEVICES="6"
 
 echo >> log.txt
-cmd="./bin/run_deblender_train.py -o -s 55 -t "16_45""
+cmd="./bin/run_deblender_train.py -o -s 42 -t "17_42""
 echo $cmd >> log.txt
 eval $cmd
