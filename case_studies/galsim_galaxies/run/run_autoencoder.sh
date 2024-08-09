4730ddec30556be624e685850a7222746fc15230
Ismael Mendoza <imendoza@umich.edu>
2024-08-09 10:30:59 -0400

    add full tag and new seed

--- case_studies/galsim_galaxies/run/run_autoencoder.sh
+++ case_studies/galsim_galaxies/run/run_autoencoder.sh
@@ -3,6 +3,6 @@
 export CUDA_VISIBLE_DEVICES="5"
 
 echo >> log.txt
-cmd="./bin/run_autoencoder_train.py -o -s 42 -t "2""
+cmd="./bin/run_autoencoder_train.py -o -s 43 -t "1_43""
 echo $cmd >> log.txt
 eval $cmd
