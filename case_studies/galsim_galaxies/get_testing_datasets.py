e9628fe014f1c431c74e8b34193f63f3e0c18655
Ismael Mendoza <imendoza@umich.edu>
2024-08-14 19:36:39 -0400

    change to correct path and function to be used when similuating individual galaxies

--- case_studies/galsim_galaxies/get_testing_datasets.py
+++ case_studies/galsim_galaxies/get_testing_datasets.py
@@ -7,13 +7,13 @@ import pytorch_lightning as pl
 import torch
 from astropy.table import Table
 
-from bliss.datasets.galsim_blends import generate_dataset
+from bliss.datasets.galsim_blends import generate_dataset, generate_individual_dataset
 from bliss.datasets.lsst import get_default_lsst_psf
 from bliss.datasets.table_utils import column_to_tensor
 from bliss.reporting import get_snr
 
 HOME_DIR = Path(__file__).parent.parent.parent
-DATA_DIR = Path(__file__).parent / "data"
+DATA_DIR = Path(__file__).parent / "data" / "data"
 cat = Table.read(HOME_DIR / "data/OneDegSq.fits")
 CATSIM_TABLE = cat[cat["i_ab"] < 27.3]
 star_mags = column_to_tensor(Table.read(HOME_DIR / "data/stars_med_june2018.fits"), "i_ab")
@@ -22,7 +22,7 @@ PSF = get_default_lsst_psf()
 
 
 @click.command()
-@click.option("--n-samples", default=10000, type=int)
+@click.option("--n-samples", default=20000, type=int)
 @click.option("-s", "--seed", default=42, type=int)
 @click.option("--mode", type=str, required=True)
 @click.option("-o", "--overwrite", is_flag=True, default=False)
@@ -36,36 +36,13 @@ def main(n_samples: int, seed: int, mode: str, overwrite: bool):
         if not overwrite and Path(dataset_file).exists():
             raise IOError("File already exists and overwrite flag is 'False'.")
 
-        dataset = generate_dataset(
-            n_samples,
-            CATSIM_TABLE,
-            STAR_MAGS,
-            psf=PSF,
-            max_n_sources=1,
-            galaxy_density=1000,
-            star_density=0,
-            slen=53,
-            bp=0,
-            max_shift=0,
-            add_galaxies_in_padding=False,
-        )
+        dataset = generate_individual_dataset(n_samples, CATSIM_TABLE, PSF, slen=53)
 
         # compute SNR
         dataset["snr"] = get_snr(dataset["noiseless"], dataset["background"])
 
         # convert everything to float and remove useless params
-        params_to_remove = {
-            "individuals",
-            "galaxy_params",
-            "star_fluxes",
-            "plocs",
-            "n_sources",
-            "fluxes",
-            "star_bools",
-            "galaxy_bools",
-        }
-        for p in params_to_remove:
-            dataset.pop(p)
+        dataset.pop("galaxy_params")
 
         for p1, q in dataset.items():
             dataset[p1] = q.float()
