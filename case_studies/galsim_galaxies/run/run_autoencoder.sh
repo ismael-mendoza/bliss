ce2d9d564170b208b2fdbd148d7bae068bcf724d
Ismael Mendoza <imendoza@umich.edu>
2024-08-19 22:53:30 -0400

    new lr ae

--- case_studies/galsim_galaxies/run/run_autoencoder.sh
+++ case_studies/galsim_galaxies/run/run_autoencoder.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES="5"
+export CUDA_VISIBLE_DEVICES="7"
 
 echo >> log.txt
-cmd="./bin/run_autoencoder_train.py -s 44 -b 512 -t "43_1""
+cmd="./bin/run_autoencoder_train.py -s 44 --lr 1e-4 -t "43_1""
 echo $cmd >> log.txt
 eval $cmd
