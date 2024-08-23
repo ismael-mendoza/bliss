8ff3f657db0004f530b3bf18b824ded5a6f06d53
Ismael Mendoza <imendoza@umich.edu>
2024-08-23 16:11:40 -0400

    need to test more learning rates here you go

--- case_studies/galsim_galaxies/run/run_autoencoder.sh
+++ case_studies/galsim_galaxies/run/run_autoencoder.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES="7"
+export CUDA_VISIBLE_DEVICES="1"
 
 echo >> log.txt
-cmd="./bin/run_autoencoder_train.py -s 44 --lr 1e-4 -t "43_1""
+cmd="./bin/run_autoencoder_train.py -s 42 --lr 1e-4 -t "44_1""
+echo $cmd >> log.txt
+eval $cmd
+
+echo >> log.txt
+cmd="./bin/run_autoencoder_train.py -s 42 --lr 1e-5 -t "44_1""
+echo $cmd >> log.txt
+eval $cmd
+
+echo >> log.txt
+cmd="./bin/run_autoencoder_train.py -s 42 --lr 5e-4 -t "44_1""
+echo $cmd >> log.txt
+eval $cmd
+
+echo >> log.txt
+cmd="./bin/run_autoencoder_train.py -s 42 --lr 1e-3 -t "44_1""
 echo $cmd >> log.txt
 eval $cmd
