493cd18190f672654c7f1dcdc9fbd82184c8419f
Ismael Mendoza <imendoza@umich.edu>
2024-09-20 16:54:03 -0400

    update

--- experiment/run/run_autoencoder.sh
+++ experiment/run/run_autoencoder.sh
@@ -3,16 +3,16 @@
 export CUDA_VISIBLE_DEVICES="2"
 
 echo >> log.txt
-cmd="./bin/run_autoencoder_train.py -s 42 --train-file ../data/datasets/ --val-file ../data/datasets/ ""
+cmd="./bin/run_autoencoder_train.py -s 42 --train-file ../data/datasets/ --val-file ../data/datasets/ "
 echo $cmd >> log.txt
 eval $cmd
 
 echo >> log.txt
-cmd="./bin/run_autoencoder_train.py -s 43 --train-file ../data/datasets/ --val-file ../data/datasets/ ""
+cmd="./bin/run_autoencoder_train.py -s 43 --train-file ../data/datasets/ --val-file ../data/datasets/ "
 echo $cmd >> log.txt
 eval $cmd
 
 echo >> log.txt
-cmd="./bin/run_autoencoder_train.py -s 44 --train-file ../data/datasets/ --val-file ../data/datasets/ ""
+cmd="./bin/run_autoencoder_train.py -s 44 --train-file ../data/datasets/ --val-file ../data/datasets/ "
 echo $cmd >> log.txt
 eval $cmd
