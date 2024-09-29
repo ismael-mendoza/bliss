16eacf0cee4ee4cbbd16c47c45eba61bd70b83b9
Ismael Mendoza <imendoza@umich.edu>
2024-09-29 16:24:44 -0400

    propagate logging

--- experiment/run/bin/run_binary_train.py
+++ experiment/run/bin/run_binary_train.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import click
-from pytorch_lightning.callbacks import EarlyStopping
 
 from bliss.encoders.binary import BinaryEncoder
 from experiment.run.training_functions import run_encoder_training
@@ -13,21 +12,34 @@ NUM_WORKERS = 0
 @click.option("-s", "--seed", required=True, type=int)
 @click.option("--train-file", required=True, type=str)
 @click.option("--val-file", required=True, type=str)
+@click.option("--ds-seed", required=True, type=int)
 @click.option("-b", "--batch-size", default=32)
 @click.option("-e", "--n-epochs", default=10)
 @click.option("--validate-every-n-epoch", default=1, type=int)
-@click.option("--val-check-interval", default=0.1, type=float, help="Fraction of training epoch")
-@click.option("--log-every-n-steps", default=16, type=int)
+@click.option("--val-check-interval", default=40, type=int, help="# of training batches")
+@click.option("--log-every-n-steps", default=10, type=int)
 def main(
     seed: int,
     train_file: str,
     val_file: str,
+    ds_seed: int,
     batch_size: int,
     n_epochs: int,
     validate_every_n_epoch: int,
     val_check_interval: int,
     log_every_n_steps: int,
 ):
+
+    # for logging
+    info = {
+        "ds_seed": ds_seed,
+        "train_file": train_file,
+        "val_file": val_file,
+        "batch_size": batch_size,
+        "n_epochs": n_epochs,
+        "validate_every_n_epoch": validate_every_n_epoch,
+        "lr": 1e-4,
+    }
 
     binary_encoder = BinaryEncoder()
 
@@ -42,6 +54,7 @@ def main(
         validate_every_n_epoch=validate_every_n_epoch,
         val_check_interval=val_check_interval,
         log_every_n_steps=log_every_n_steps,
+        log_info_dict=info,
     )
 
 
