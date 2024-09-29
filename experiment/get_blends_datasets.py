d07730537c212e1b959bcd0785cfc543b3dfd304
Ismael Mendoza <imendoza@umich.edu>
2024-09-29 16:20:49 -0400

    no tag, simplify logging

--- experiment/get_blends_datasets.py
+++ experiment/get_blends_datasets.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 import datetime
-from pathlib import Path
 
 import click
 import numpy as np
 import pytorch_lightning as L
 import torch
 
+from bliss import DATASETS_DIR, HOME_DIR
 from bliss.datasets.generate_blends import generate_dataset
 from bliss.datasets.lsst import (
     GALAXY_DENSITY,
@@ -17,19 +17,14 @@ from bliss.datasets.lsst import (
     prepare_final_star_catalog,
 )
 
-HOME_DIR = Path(__file__).parent.parent.parent
-DATASETS_DIR = Path("/nfs/turbo/lsa-regier/scratch/ismael/datasets/")
-LOG_FILE = HOME_DIR / "experiment/run/log.txt"
-
+LOG_FILE = HOME_DIR / "experiment/log.txt"
 
 CATSIM_CAT = prepare_final_galaxy_catalog()
 STAR_MAGS = prepare_final_star_catalog()
 
 PSF = get_default_lsst_psf()
 
-TAG = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
-assert DATASETS_DIR.exists()
 assert LOG_FILE.exists()
 
 
@@ -43,21 +38,9 @@ def main(seed: int, n_samples: int, galaxy_density: float, star_density: float):
     L.seed_everything(seed)
     rng = np.random.default_rng(seed)  # for catalog indices
 
-    train_ds_file = DATASETS_DIR / f"train_ds_{seed}_{TAG}.pt"
-    val_ds_file = DATASETS_DIR / f"val_ds_{seed}_{TAG}.pt"
-    test_ds_file = DATASETS_DIR / f"test_ds_{seed}_{TAG}.pt"
-
-    with open(LOG_FILE, "a") as f:
-        now = datetime.datetime.now()
-        print("", file=f)
-        log_msg = f"""Run training blend data generation script...
-        With seed {seed} at {now}
-        Galaxy density {galaxy_density}, star_density {star_density}, and n_samples {n_samples}.
-        Samples will be divided into 3 datasets of {n_samples} # of blends.
-
-        With TAG: {TAG}
-        """
-        print(log_msg, file=f)
+    train_ds_file = DATASETS_DIR / f"train_ds_{seed}.pt"
+    val_ds_file = DATASETS_DIR / f"val_ds_{seed}.pt"
+    test_ds_file = DATASETS_DIR / f"test_ds_{seed}.pt"
 
     # disjointed tables with different galaxies
     n_rows = len(CATSIM_CAT)
@@ -86,6 +69,15 @@ def main(seed: int, n_samples: int, galaxy_density: float, star_density: float):
             max_shift=0.5,
         )
         torch.save(ds, f)
+
+    # logging
+    with open(LOG_FILE, "a") as f:
+        now = datetime.datetime.now()
+        print("", file=f)
+        log_msg = f"""\nBlend data generation with seed {seed} at {now}.
+        Galaxy density {galaxy_density}, star_density {star_density}, and n_samples {n_samples}.
+        """
+        print(log_msg, file=f)
 
 
 if __name__ == "__main__":
