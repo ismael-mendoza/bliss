f09a78ade43680a819a49e0bed4ca91f7f34f16e
Ismael Mendoza <imendoza@umich.edu>
2024-09-30 16:47:14 -0400

    only using chunks seems to work everytime

--- bliss/datasets/io.py
+++ bliss/datasets/io.py
@@ -2,22 +2,19 @@
 
 # useful resource: https://gist.github.com/gilliss/7e1d451b42441e77ae33a8b790eeeb73
 
-import os
 from pathlib import Path
 
 import h5py
 import torch
 from torch import Tensor
 
-os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
-
 
 def save_dataset_h5py(ds: dict[str, Tensor], fpath: str | Path) -> None:
     assert not Path(fpath).exists(), "overwriting existing ds"
     assert Path(fpath).suffix in {".hdf5", ".h5"}
-    with h5py.File(fpath, "w", locking=False) as f:
+    with h5py.File(fpath, "w") as f:
         for k, v in ds.items():
-            f.create_dataset(k, data=v.numpy())
+            f.create_dataset(k, data=v.numpy(), chunks=True)
 
 
 def load_dataset_h5py(fpath: str | Path) -> dict[str, Tensor]:
