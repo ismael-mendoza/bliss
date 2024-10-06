9086d29583c7bba41d102657b63f3076de9ea31d
Ismael Mendoza <imendoza@umich.edu>
2024-10-06 17:44:36 -0400

    validation takes too long with bigger dataset, check once every epoch, hopefully regularization makes up for it

--- experiment/run/bin/run_detection_train.py
+++ experiment/run/bin/run_detection_train.py
@@ -7,23 +7,21 @@ from experiment.run.training_functions import run_encoder_training
 
 @click.command()
 @click.option("-s", "--seed", required=True, type=int)
+@click.option("--ds-seed", required=True, type=int, help="Random seed used for dataset")
 @click.option("--train-file", required=True, type=str)
 @click.option("--val-file", required=True, type=str)
-@click.option("--ds-seed", required=True, type=int, help="Random seed used for dataset")
 @click.option("-b", "--batch-size", default=32)
 @click.option("-e", "--n-epochs", default=25)
 @click.option("--validate-every-n-epoch", default=1, type=int)
-@click.option("--val-check-interval", default=40, type=int, help="# of training batches")
-@click.option("--log-every-n-steps", default=10, type=int)
+@click.option("--log-every-n-steps", default=50, type=int)
 def main(
     seed: int,
+    ds_seed: int,
     train_file: str,
     val_file: str,
-    ds_seed: int,
     batch_size: int,
     n_epochs: int,
     validate_every_n_epoch: int,
-    val_check_interval: int,
     log_every_n_steps: int,
 ):
 
@@ -35,7 +33,7 @@ def main(
         "batch_size": batch_size,
         "n_epochs": n_epochs,
         "validate_every_n_epoch": validate_every_n_epoch,
-        "val_check_interval": val_check_interval,
+        "val_check_interval": None,
         "lr": 1e-4,
     }
 
@@ -49,7 +47,7 @@ def main(
         model=model,
         model_name="detection",
         validate_every_n_epoch=validate_every_n_epoch,
-        val_check_interval=val_check_interval,
+        val_check_interval=None,
         log_every_n_steps=log_every_n_steps,
         log_info_dict=info,
     )
