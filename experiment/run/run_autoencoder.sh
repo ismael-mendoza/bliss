80547d1327b83356bca42c08432bdcc5174bc722
Ismael Mendoza <imendoza@umich.edu>
2024-10-06 17:43:13 -0400

    fix these small scripts with new format for datsets

--- experiment/run/run_autoencoder.sh
+++ experiment/run/run_autoencoder.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 export CUDA_VISIBLE_DEVICES="3"
+export SEED="42"
 
-./bin/run_autoencoder_train.py --seed 41 \
---train-file ../data/datasets/train_ae_ds_42_20240920164841.pt \
---val-file ../data/datasets/val_ae_ds_42_20240920164841.pt"
+./run_autoencoder_train.py --seed $SEED --ds-seed $SEED --train-file ../data/datasets/train_ae_ds_${SEED}.npz --val-file ../data/datasets/val_ae_ds_${SEED}.npz
--- experiment/run/run_detection.sh
+++ experiment/run/run_detection.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES="0"
+export CUDA_VISIBLE_DEVICES="1"
 
-./bin/run_detection_train.py --seed 42 --ds-seed 42 --train-file ../data/datasets/train_ds_42.hdf5 --val-file ../data/datasets/val_ds_42.hdf5
+./bin/run_detection_train.py --seed 42 --ds-seed 42 --train-file ../data/datasets/train_ds_42.npz --val-file ../data/datasets/val_ds_42.npz
