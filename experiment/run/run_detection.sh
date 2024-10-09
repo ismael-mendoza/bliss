e6b570b90eca95c720f7e614cb6b98b72f846334
Ismael Mendoza <imendoza@umich.edu>
2024-10-09 18:12:01 -0400

    no changes

--- experiment/run/run_detection.sh
+++ experiment/run/run_detection.sh
@@ -2,4 +2,4 @@
 
 export CUDA_VISIBLE_DEVICES="1"
 
-./bin/run_detection_train.py --seed 42 --ds-seed 42 --train-file ../data/datasets/train_ds_42.npz --val-file ../data/datasets/val_ds_42.npz
+./bin/run_detection_train.py --seed 43 --ds-seed 42 --train-file ../data/datasets/train_ds_42.npz --val-file ../data/datasets/val_ds_42.npz
