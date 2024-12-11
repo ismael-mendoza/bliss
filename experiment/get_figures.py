cb92dc6f278c594fbb9694e3af6a3dd5fb470e7d
Ismael Mendoza <imendoza@umich.edu>
2024-12-11 16:34:46 -0500

    overhaul figures

--- experiment/get_figures.py
+++ experiment/get_figures.py
@@ -1,92 +1,61 @@
 #!/usr/bin/env python3
 import warnings
-from copy import deepcopy
 from pathlib import Path
 
-import click
 import pytorch_lightning as pl
 import torch
+import typer
 
-from bliss.encoders.autoencoder import OneCenteredGalaxyAE
 from bliss.encoders.binary import BinaryEncoder
 from bliss.encoders.deblend import GalaxyEncoder
 from bliss.encoders.detection import DetectionEncoder
-from bliss.encoders.encoder import Encoder
-from experiment.scripts_figures.ae_figures import AutoEncoderFigures
 from experiment.scripts_figures.detection_figures import BlendDetectionFigures
-from experiment.scripts_figures.toy_figures import ToySeparationFigure
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-ALL_FIGS = ("single", "detection", "deblend", "toy")
+ALL_FIGS = ("detection", "deblend", "toy")
 CACHEDIR = "/nfs/turbo/lsa-regier/scratch/ismael/cache/"
 
 
-def _load_models(seed: int, ds_seed: int, device):
+def _load_models(fpaths: dict[str, str], model: str, device):
     """Load models required for producing results."""
 
-    # encoders
-    detection = DetectionEncoder().to(device).eval()
-    detection.load_state_dict(
-        torch.load(f"models/detection_{ds_seed}_{seed}.pt", map_location=device, weights_only=True)
-    )
-    detection.requires_grad_(False)
+    if model == "detection":
+        detection_fpath = fpaths["detection"]
+        detection = DetectionEncoder().to(device).eval()
+        detection.load_state_dict(
+            torch.load(detection_fpath, map_location=device, weights_only=True)
+        )
+        detection.requires_grad_(False)
+        return detection
 
-    binary = BinaryEncoder().to(device).eval()
-    binary.load_state_dict(
-        torch.load(f"models/binary_{ds_seed}_{seed}.pt", map_location=device, weights_only=True)
-    )
-    binary.requires_grad_(False)
+    elif model == "binary":
+        binary_fpath = fpaths["binary"]
+        binary = BinaryEncoder().to(device).eval()
+        binary.load_state_dict(torch.load(binary_fpath, map_location=device, weights_only=True))
+        binary.requires_grad_(False)
+        return binary
 
-    deblender = GalaxyEncoder(f"models/autoencoder_{ds_seed}_{seed}.pt")
-    deblender.load_state_dict(
-        torch.load(f"models/deblender_{ds_seed}_{seed}.pt", map_location=device, weights_only=True)
-    )
-    deblender.requires_grad_(False)
+    elif model == "deblend":
+        deblend_fpath = fpaths["deblend"]
+        ae_fpath = fpaths["ae"]
 
-    encoder = Encoder(
-        detection.eval(),
-        binary.eval(),
-        deblender.eval(),
-        n_images_per_batch=20,
-        n_rows_per_batch=30,
-    )
-    encoder = encoder.to(device)
+        deblender = GalaxyEncoder(ae_fpath)
+        deblender.load_state_dict(torch.load(deblend_fpath, map_location=device, weights_only=True))
+        deblender.requires_grad_(False)
+        return deblender
 
-    # decoder
-    ae = OneCenteredGalaxyAE().to(device).eval()
-    ae.load_state_dict(torch.load(f"models/autoencoder_{ds_seed}_{seed}.pt", map_location=device))
-    decoder = deepcopy(ae.dec)
-    decoder.requires_grad_(False)
-    decoder = decoder.eval()
-    del ae
-
-    return encoder, decoder
-
-
-def _make_autoencoder_figures(seed: int, ds_seed: int, device, test_file: str, overwrite: bool):
-    print("INFO: Creating autoencoder figures...")
-    suffix = f"{ds_seed}_{seed}"
-    autoencoder = OneCenteredGalaxyAE()
-    autoencoder.load_state_dict(torch.load(f"models/autoencoder_{suffix}.pt", weights_only=True))
-    autoencoder = autoencoder.to(device).eval()
-    autoencoder.requires_grad_(False)
-
-    # arguments for figures
-    args = (autoencoder, test_file)
-
-    # create figure classes and plot.
-    AutoEncoderFigures(
-        n_examples=5,
-        overwrite=overwrite,
-        figdir="figures",
-        cachedir=CACHEDIR,
-        suffix=suffix,
-    )(*args)
+    raise ValueError(f"model name {model} is not recognized.")
 
 
 def _make_detection_figure(
-    encoder: Encoder, test_file: str, *, aperture: float, suffix: str, overwrite: bool
+    fpaths: dict[str, str],
+    test_file: str,
+    *,
+    aperture: float,
+    suffix: str,
+    overwrite: bool,
+    device,
 ):
     print("INFO: Creating figures for detection encoder performance simulated blended galaxies.")
     _init_kwargs = {
@@ -96,53 +65,54 @@ def _make_detection_figure(
         "cachedir": CACHEDIR,
         "aperture": aperture,
     }
-    BlendDetectionFigures(**_init_kwargs)(detection=encoder.detection_encoder, ds_path=test_file)
+    detection = _load_models(fpaths, "detection", device)
+    BlendDetectionFigures(**_init_kwargs)(detection=detection, ds_path=test_file)
 
 
-@click.command()
-@click.option("-m", "--mode", required=True, type=click.Choice(ALL_FIGS, case_sensitive=False))
-@click.option("-s", "--seed", required=True, type=int, help="Consistent seed used to train models.")
-@click.option("--ds-seed", required=True, type=int, help="Seed of training/testing set.")
-@click.option("--aperture", default=5.0, type=float, help="Aperture radius for results.")
-@click.option("--test-file-single", default="", type=str, help="Dataset file for testing AE.")
-@click.option("--test-file-blends", default="", type=str, help="Dataset file for testing Encoders.")
-@click.option("-o", "--overwrite", is_flag=True, default=False, help="Whether to overwrite cache.")
 def main(
     mode: str,
-    seed: int,
-    ds_seed: int,
-    aperture: float,
-    test_file_single: str,
-    test_file_blends: str,
-    overwrite: bool,
+    suffix: str,
+    seed: int = 42,
+    aperture: float = 5.0,
+    test_file_single: str = "",
+    test_file_blends: str = "",
+    detection_fpath: str = "",
+    deblend_fpath: str = "",
+    binary_fpath: str = "",
+    ae_fpath: str = "",
+    overwrite: bool = False,
 ):
     assert mode in ALL_FIGS
-    suffix = f"{ds_seed}_{seed}"
 
     device = torch.device("cuda:0")
     pl.seed_everything(seed)
 
-    # FIGURE 1: Autoencoder single galaxy reconstruction
-    if mode == "single":
-        assert test_file_single != "" and Path(test_file_single).exists()
-        _make_autoencoder_figures(seed, ds_seed, device, test_file_single, overwrite)
-
-    if mode in {"toy", "detection", "deblend", "classification"}:
-        encoder, decoder = _load_models(seed, ds_seed, device)
+    fpaths = {
+        "detection": detection_fpath,
+        "binary": binary_fpath,
+        "deblend": deblend_fpath,
+        "ae": ae_fpath,
+    }
 
     if mode == "detection":
         assert test_file_blends != "" and Path(test_file_blends).exists()
+        assert detection_fpath != "" and Path(detection_fpath).exists()
         _make_detection_figure(
-            encoder, test_file_blends, suffix=suffix, overwrite=overwrite, aperture=aperture
+            fpaths,
+            test_file_blends,
+            suffix=suffix,
+            overwrite=overwrite,
+            aperture=aperture,
+            device=device,
         )
 
-    if mode == "toy":
-        print("INFO: Creating figures for testing BLISS on pair galaxy toy example.")
-        cachedir = "/nfs/turbo/lsa-regier/scratch/ismael/cache/"
-        ToySeparationFigure(
-            overwrite=overwrite, figdir="figures", cachedir=cachedir, suffix=suffix
-        )(encoder, decoder)
+    # if mode == "toy":
+    #     print("INFO: Creating figures for testing BLISS on pair galaxy toy example.")
+    #     cachedir = "/nfs/turbo/lsa-regier/scratch/ismael/cache/"
+    #     ToySeparationFigure(
+    #         overwrite=overwrite, figdir="figures", cachedir=cachedir, suffix=suffix
+    #     )(encoder, decoder)
 
 
 if __name__ == "__main__":
-    main()  # pylint: disable=no-value-for-parameter
+    typer.run(main)
