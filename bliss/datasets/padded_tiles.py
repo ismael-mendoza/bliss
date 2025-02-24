3013b4a7a4d7d044851524f96088356b559696aa
Ismael Mendoza <imendoza@umich.edu>
2025-02-24 16:32:31 -0500

    reorganize

--- bliss/datasets/padded_tiles.py
+++ bliss/datasets/padded_tiles.py
@@ -26,42 +26,6 @@ from bliss.datasets.render_utils import (
 )
 
 
-def render_padded_image(
-    catsim_table: Table,
-    all_star_mags: np.ndarray,
-    mean_sources: float,
-    galaxy_prob: float,
-    psf: galsim.GSObject,
-    slen: int,
-    bp: int,
-):
-    """Return noiseless image of galaxies only in padding (centroid outside `slen`)."""
-    size = slen + 2 * bp
-    n_sources = sample_poisson_n_sources(mean_sources, torch.inf)
-    image = torch.zeros((1, size, size))
-
-    # we don't need to record or keep track, just produce the image in padding
-    # we will construct the image galaxy by galaxy
-    for _ in range(n_sources):
-        # offset always needs to be out of the central square
-        x, y = uniform_out_of_square(slen, size)
-        offset = torch.tensor([x, y])
-
-        is_galaxy = sample_bernoulli(galaxy_prob, 1).bool().item()
-        if is_galaxy:
-            params, _ = sample_galaxy_params(catsim_table, 1, 1)
-            assert params.shape == (1, 11)
-            one_galaxy_params = params[0]
-            galaxy = render_one_galaxy(one_galaxy_params, psf, size, offset)
-            image += galaxy
-        else:
-            star_flux = sample_star_fluxes(all_star_mags, 1, 1).item()
-            star = render_one_star(psf, star_flux, size, offset)
-            image += star
-
-    return image
-
-
 def generate_padded_tiles(
     n_samples: int,
     catsim_table: Table,
@@ -173,7 +137,43 @@ def generate_padded_tiles(
     }
 
 
-def parse_dataset(dataset: dict[str, Tensor]):
+def render_padded_image(
+    catsim_table: Table,
+    all_star_mags: np.ndarray,
+    mean_sources: float,
+    galaxy_prob: float,
+    psf: galsim.GSObject,
+    slen: int,
+    bp: int,
+):
+    """Return noiseless image of galaxies only in padding (centroid outside `slen`)."""
+    size = slen + 2 * bp
+    n_sources = sample_poisson_n_sources(mean_sources, torch.inf)
+    image = torch.zeros((1, size, size))
+
+    # we don't need to record or keep track, just produce the image in padding
+    # we will construct the image galaxy by galaxy
+    for _ in range(n_sources):
+        # offset always needs to be out of the central square
+        x, y = uniform_out_of_square(slen, size)
+        offset = torch.tensor([x, y])
+
+        is_galaxy = sample_bernoulli(galaxy_prob, 1).bool().item()
+        if is_galaxy:
+            params, _ = sample_galaxy_params(catsim_table, 1, 1)
+            assert params.shape == (1, 11)
+            one_galaxy_params = params[0]
+            galaxy = render_one_galaxy(one_galaxy_params, psf, size, offset)
+            image += galaxy
+        else:
+            star_flux = sample_star_fluxes(all_star_mags, 1, 1).item()
+            star = render_one_star(psf, star_flux, size, offset)
+            image += star
+
+    return image
+
+
+def parse_ptiles_dataset(dataset: dict[str, Tensor]):
     """Parse dataset into a tuple of (images, TileCatalog)."""
     params = dataset.copy()  # make a copy to not change argument.
     ptiles = params.pop("images")
