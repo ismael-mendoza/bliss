cc8b58d4c24a8f71a700d43f710296dbdc218fbf
Ismael Mendoza <imendoza@umich.edu>
2025-02-25 12:54:25 -0500

    bug fixes

--- bliss/datasets/saved_datasets.py
+++ bliss/datasets/saved_datasets.py
@@ -18,21 +18,21 @@ class SavedGalsimBlends(Dataset):
 
         self.images = ds.pop("images")
         self.epoch_size = len(self.images)
+        self.is_deblender = is_deblender
 
         if not is_deblender:
             ds.pop("uncentered_sources")
             ds.pop("paddings")
             ds.pop("centered_sources")
-            self.centered_sources = torch.tensor([0]).float()
+            self.centered = torch.tensor([0]).float()
         else:
             noise = self.images - ds.pop("uncentered_sources") - ds.pop("paddings")
-            centered = ds.pop("centered") + noise
+            centered = ds.pop("centered_sources") + noise
             self.centered = centered
             ds.pop("n_sources")
 
         ds.pop("galaxy_params")
         ds.pop("star_fluxes")
-        ds.pop("star_bools")
 
         self.tile_params = {**ds}
 
@@ -43,7 +43,7 @@ class SavedGalsimBlends(Dataset):
         tile_params_ii = {p: q[index] for p, q in self.tile_params.items()}
         return {
             "images": self.images[index],
-            "centered": self.centered[index],
+            "centered": self.centered[index] if self.is_deblender else self.centered,
             **tile_params_ii,
         }
 
