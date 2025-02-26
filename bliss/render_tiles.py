2f6ef377f0adec14d0837568d06e5099113c0531
Ismael Mendoza <imendoza@umich.edu>
2025-02-26 16:02:10 -0500

    new render from decoder with galsim

--- bliss/render_tiles.py
+++ bliss/render_tiles.py
@@ -1,7 +1,8 @@
 """Functions from producing images from tiled parameters of galaxies or stars."""
 
 import torch
-from einops import rearrange
+from einops import pack, rearrange, unpack
+from galsim import InterpolatedImage
 from torch import Tensor
 from torch.nn.functional import fold, unfold
 
@@ -13,7 +14,6 @@ def render_galaxy_ptiles(
     locs: Tensor,
     galaxy_params: Tensor,
     galaxy_bools: Tensor,
-    ptile_slen: int,
     tile_slen: int,
     *,
     n_bands: int = 1,
@@ -32,16 +32,9 @@ def render_galaxy_ptiles(
     )
     assert centered_galaxies.shape[-1] == galaxy_decoder.slen
     assert galaxy_decoder.slen % 2 == 1  # so centered in central pixel
-
-    # render galaxies in correct location within padded tile and trim to be size `ptile_slen`
-    uncentered_galaxies = shift_sources_bilinear(
-        centered_galaxies,
-        locs_flat,
-        tile_slen=tile_slen,
-        slen=ptile_slen,
-        center=False,
+    uncentered_galaxies = _shift_sources_galsim(
+        centered_galaxies, locs_flat, galaxy_bools_flat, tile_slen=tile_slen, center=False
     )
-    assert uncentered_galaxies.shape[-1] == ptile_slen
 
     return rearrange(
         uncentered_galaxies, "(b nth ntw) c h w -> b nth ntw c h w", b=b, nth=nth, ntw=ntw
@@ -70,6 +63,38 @@ def _render_centered_galaxies_ptiles(
 
     # be extra careful
     return gal * rearrange(is_gal, "npt -> npt 1 1 1")
+
+
+def _shift_sources_galsim(
+    ptiles_flat: Tensor,
+    locs: Tensor,
+    galaxy_bools: Tensor,
+    *,
+    tile_slen: int,
+    center: bool = False,
+    scale: float = 0.2,
+):
+    """Use interpolation to shift noiseless reconstruction ptiles that contain galaxies."""
+    assert locs.ndim == galaxy_bools.ndim == 2
+    assert ptiles_flat.shape[1] == 1
+    npt = ptiles_flat.shape[0]
+    slen = ptiles_flat.shape[-1]
+    new_ptiles = torch.zeros_like(ptiles_flat)
+    locs_trans = swap_locs_columns(locs)
+    sgn = -1 if center else 1
+
+    ptiles_flat_np = ptiles_flat.numpy()
+    for ii in range(npt):
+        if galaxy_bools[ii].bool().item():
+            image = ptiles_flat_np[ii, 0]
+            xy = locs_trans[ii]
+            offset = (xy * tile_slen - tile_slen / 2) * sgn
+
+            iimg = InterpolatedImage(image, scale=scale)
+            fimg = iimg.drawImage(nx=slen, ny=slen, scale=scale, offset=offset, method="no_pixel")
+            new_ptiles[ii, 0] = torch.from_numpy(fimg)
+
+    return new_ptiles
 
 
 def reconstruct_image_from_ptiles(image_ptiles: Tensor, tile_slen: int) -> Tensor:
@@ -148,3 +173,10 @@ def validate_border_padding(tile_slen: int, ptile_slen: int, bp: int | None = No
         bp = (ptile_slen - tile_slen) / 2
     assert float(bp).is_integer(), "amount of border padding must be an integer"
     return int(bp)
+
+
+def swap_locs_columns(locs: Tensor) -> Tensor:
+    """Swap the columns of locs to invert 'x' and 'y' with einops!"""
+    assert locs.ndim == 2 and locs.shape[1] == 2
+    x, y = unpack(locs, [[1], [1]], "b *")
+    return pack([y, x], "b *")[0]
