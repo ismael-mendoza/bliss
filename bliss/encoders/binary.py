ef7ee36342f26db08b5f4e1fe08100e98daf1163
Ismael Mendoza <imendoza@umich.edu>
2025-02-26 16:04:26 -0500

    no centering

--- bliss/encoders/binary.py
+++ bliss/encoders/binary.py
@@ -8,8 +8,7 @@ from torch.optim import Adam
 from bliss.catalog import TileCatalog
 from bliss.datasets.generate_blends import parse_dataset
 from bliss.encoders.layers import EncoderCNN, make_enc_final
-from bliss.grid import shift_sources_bilinear, validate_border_padding
-from bliss.render_tiles import get_images_in_tiles
+from bliss.render_tiles import get_images_in_tiles, validate_border_padding
 
 
 class BinaryEncoder(pl.LightningModule):
@@ -48,9 +47,8 @@ class BinaryEncoder(pl.LightningModule):
         self.tile_slen = tile_slen
         self.ptile_slen = ptile_slen
         self.bp = validate_border_padding(tile_slen, ptile_slen)
-        self.final_slen = self.ptile_slen - 2 * self.tile_slen  # will always crop 2 * tile_slen
 
-        dim_enc_conv_out = ((self.final_slen + 1) // 2 + 1) // 2
+        dim_enc_conv_out = ((self.ptile_slen + 1) // 2 + 1) // 2
         self._enc_conv = EncoderCNN(n_bands, channel, spatial_dropout)
         self._enc_final = make_enc_final(channel * 4 * dim_enc_conv_out**2, hidden, 1, dropout)
 
@@ -65,8 +63,7 @@ class BinaryEncoder(pl.LightningModule):
 
     def encode_tiled(self, image_ptiles_flat: Tensor, flat_locs: Tensor):
         npt, _ = flat_locs.shape
-        centered_tiles = self._center_ptiles(image_ptiles_flat, flat_locs)
-        x = rearrange(centered_tiles, "npt c h w -> npt c h w")
+        x = rearrange(image_ptiles_flat, "npt c h w -> npt c h w")
         h = self._enc_conv(x)
         h2 = self._enc_final(h)
         galaxy_probs = torch.sigmoid(h2).clamp(1e-4, 1 - 1e-4)
@@ -120,21 +117,3 @@ class BinaryEncoder(pl.LightningModule):
 
     def configure_optimizers(self):
         return Adam(self.parameters(), lr=1e-4)
-
-    def _center_ptiles(self, image_ptiles: Tensor, tile_locs_flat: Tensor) -> Tensor:
-        assert image_ptiles.shape[-1] == image_ptiles.shape[-2] == self.ptile_slen
-        shifted_ptiles = shift_sources_bilinear(
-            image_ptiles,
-            tile_locs_flat,
-            tile_slen=self.tile_slen,
-            slen=self.ptile_slen,
-            center=True,
-        )
-        assert shifted_ptiles.shape[-1] == shifted_ptiles.shape[-2] == self.ptile_slen
-        cropped_ptiles = shifted_ptiles[
-            ...,
-            self.tile_slen : (self.ptile_slen - self.tile_slen),
-            self.tile_slen : (self.ptile_slen - self.tile_slen),
-        ]
-        assert cropped_ptiles.shape[-1] == cropped_ptiles.shape[-2] == self.final_slen
-        return cropped_ptiles
