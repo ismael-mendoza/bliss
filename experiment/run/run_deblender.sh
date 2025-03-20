5d7dab0fc83044fcc46bb2727d1abe9796980ba4
Ismael Mendoza <imendoza@umich.edu>
2025-03-20 16:10:26 -0400

    shell

--- experiment/run/run_deblender.sh
+++ experiment/run/run_deblender.sh
@@ -2,4 +2,4 @@
 
 export CUDA_VISIBLE_DEVICES="5"
 
-./bin/run_deblender_train.py --seed 22 --ds-seed 23 --ae-model-path ../models/autoencoder_42_42.pt --train-file ../data/datasets/train_ds_deblend_23.npz --val-file ../data/datasets/val_ds_deblend_23.npz
+./bin/run_deblender_train.py --seed 23 --ds-seed 23 --ae-model-path ../models/autoencoder_42_42.pt --train-file ../data/datasets/train_ds_deblend_23.npz --val-file ../data/datasets/val_ds_deblend_23.npz
