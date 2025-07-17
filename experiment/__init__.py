3e16dc68a7af7000235ce771f8ceee2e5ddc77d2
Ismael Mendoza <imendoza@umich.edu>
2025-07-17 11:27:00 -0400

    more organizing

--- experiment/__init__.py
+++ experiment/__init__.py
@@ -2,8 +2,12 @@
 
 from pathlib import Path
 
+SEED = 52
+
 DATASETS_DIR = Path("/nfs/turbo/lsa-regier/scratch/ismael/datasets/")
 CACHE_DIR = Path("/nfs/turbo/lsa-regier/scratch/ismael/cache/")
-FIGURE_DIR = Path("/home/imendoza/bliss/experiment/figures")
-MODELS_DIR = Path("/home/imendoza/bliss/experiment/models")
-SEED = 52
+FIGURE_DIR = Path("/home/imendoza/bliss/experiment/figures/")
+MODELS_DIR = Path("/home/imendoza/bliss/experiment/models/")
+
+# metadata for models saved for pytorch lightning
+TORCH_DIR = Path("/nfs/turbo/lsa-regier/scratch/ismael/out/")
