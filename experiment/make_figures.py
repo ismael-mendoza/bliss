6cf55a11a170ae20b16a3fe4dcd8ae26b6b32abd
Ismael Mendoza <imendoza@umich.edu>
2025-07-19 17:30:29 -0400

    fix script

--- experiment/make_figures.py
+++ experiment/make_figures.py
@@ -19,25 +19,26 @@ def main(
     L.seed_everything(SEED)
     FIGURE_DIR.mkdir(exist_ok=True)
     CACHE_DIR.mkdir(exist_ok=True)
+    overwrite_txt = "--overwrite" if overwrite else "--no-overwrite"
 
     if detection or all:
-        cmd = f"./scripts/get_figures.py --mode detection --overwrite {overwrite}"
+        cmd = f"./scripts/get_figures.py --mode detection {overwrite_txt}"
         subprocess.check_call(cmd, shell=True)
 
     if binary or all:
-        cmd = f"./scripts/get_figures.py --mode binary --overwrite {overwrite}"
+        cmd = f"./scripts/get_figures.py --mode binary {overwrite_txt}"
         subprocess.check_call(cmd, shell=True)
 
     if deblend or all:
-        cmd = f"./scripts/get_figures.py --mode deblend --overwrite {overwrite}"
+        cmd = f"./scripts/get_figures.py --mode deblend {overwrite_txt}"
         subprocess.check_call(cmd, shell=True)
 
     if toy or all:
-        cmd = f"./scripts/get_figures.py --mode toy --overwrite {overwrite}"
+        cmd = f"./scripts/get_figures.py --mode toy {overwrite_txt}"
         subprocess.check_call(cmd, shell=True)
 
     if samples or all:
-        cmd = f"./scripts/get_figures.py --mode samples --overwrite {overwrite}"
+        cmd = f"./scripts/get_figures.py --mode samples {overwrite_txt}"
         subprocess.check_call(cmd, shell=True)
 
 
